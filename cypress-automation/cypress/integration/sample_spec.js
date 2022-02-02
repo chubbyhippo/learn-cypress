26f302cf1deaf971869911e7527a7918f511e10d
ch <mkmsacc@outlook.com>
2022-02-02 14:44:29 +0700

    Add wait for 2 secs

--- cypress-automation/cypress/integration/sample_spec.js
+++ cypress-automation/cypress/integration/sample_spec.js
@@ -2,6 +2,8 @@ describe("My First Test Suite", function () {
     it("My First Test case", function () {
         cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
         cy.get('.search-keyword').type('ca')
+        cy.wait(2000)
+        cy.get('.product').should("have.length", 4)
     })
 
 })
