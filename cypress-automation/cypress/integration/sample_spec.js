427e0936058389de574e3d2ae1d6d2f8a777ebca
ch <mkmsacc@outlook.com>
2022-02-03 12:16:43 +0700

    Format code

--- cypress-automation/cypress/integration/sample_spec.js
+++ cypress-automation/cypress/integration/sample_spec.js
@@ -1,9 +1,10 @@
 describe("My First Test Suite", function () {
     it("My First Test case", function () {
-        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
-        cy.get('.search-keyword').type('ca')
-        cy.wait(2000)
-        cy.get('.product:visible').should("have.length", 4)
-        cy.get('.products').find('.product').should('have.length', 4)
-    })
-})
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+        cy.get(".search-keyword").type("ca");
+        cy.wait(2000);
+        cy.get(".product:visible").should("have.length", 4);
+        cy.get(".products").find(".product").should("have.length", 4);
+
+    });
+});
