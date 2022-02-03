fd131d22ad13fb47116813716ea3e2d58019cb93
ch <mkmsacc@outlook.com>
2022-02-03 13:59:39 +0700

    Add click events

--- cypress-automation/cypress/integration/sample_spec.js
+++ cypress-automation/cypress/integration/sample_spec.js
@@ -6,6 +6,9 @@ describe("My First Test Suite", function () {
         cy.get(".product").should("have.length", 5);
         cy.get(".product:visible").should("have.length", 4);
         cy.get(".products").find(".product").should("have.length", 4);
+        cy.get(":nth-child(1) > .stepper-input > .increment").click();
+        cy.get(":nth-child(1) > .stepper-input > .increment").click();
+        cy.get(".products").find(".product").eq(0).contains("ADD TO CART").click();
 
     });
 });
